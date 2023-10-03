a519a2921db1403f7b358705a1d0a25aaea1a856
Miguel Duarte Barroso <mdbarroso@redhat.com>
2023-10-03 18:29:39 +0200

    macvlan: allow specifying MAC on iface ADD
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- examples/create_macvlan.rs
+++ examples/create_macvlan.rs
@@ -1,22 +1,33 @@
 // SPDX-License-Identifier: MIT
 
 use futures::stream::TryStreamExt;
+use macaddr::MacAddr;
 use rtnetlink::{new_connection, Error, Handle};
-use std::env;
+use std::{env, str::FromStr};
+
+use netlink_packet_route::link::LinkAttribute;
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() != 2 && args.len() != 3 {
         usage();
         return Ok(());
     }
     let link_name = &args[1];
+    let mac: Option<Vec<u8>> = if args.len() == 3 {
+        let mac_address_arg = (&args[2]).to_string();
+        let mac_address = MacAddr::from_str(mac_address_arg.as_str())
+            .map_err(|e| format!("{e}"))?;
+        Some(mac_address.as_bytes().into())
+    } else {
+        None
+    };
 
     let (connection, handle, _) = new_connection().unwrap();
     tokio::spawn(connection);
 
-    create_macvlan(handle, link_name.to_string())
+    create_macvlan(handle, link_name.to_string(), mac)
         .await
         .map_err(|e| format!("{e}"))
 }
@@ -24,14 +35,21 @@ async fn main() -> Result<(), String> {
 async fn create_macvlan(
     handle: Handle,
     link_name: String,
+    mac_address: Option<Vec<u8>>,
 ) -> Result<(), Error> {
     let mut links = handle.link().get().match_name(link_name.clone()).execute();
     if let Some(link) = links.try_next().await? {
-        let request = handle.link().add().macvlan(
+        let mut request = handle.link().add().macvlan(
             "test_macvlan".into(),
             link.header.index,
             4u32, // bridge mode
         );
+        if let Some(mac) = mac_address {
+            request
+                .message_mut()
+                .attributes
+                .push(LinkAttribute::Address(mac));
+        }
         request.execute().await?
     } else {
         println!("no link {link_name} found");
@@ -42,7 +60,7 @@ async fn create_macvlan(
 fn usage() {
     eprintln!(
         "usage:
-    cargo run --example create_macvlan -- <link name>
+    cargo run --example create_macvlan -- <link name> [mac address]
 
 Note that you need to run this program as root. Instead of running cargo as root,
 build the example normally:
@@ -51,6 +69,6 @@ build the example normally:
 
 Then find the binary in the target directory:
 
-    cd target/debug/examples ; sudo ./create_macvlan <link_name>"
+    cd target/debug/examples ; sudo ./create_macvlan <link_name> [mac address]"
     );
 }
