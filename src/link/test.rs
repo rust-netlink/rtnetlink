ed35e89ff4543ba9e552d1837e7797dff5e8cbf1
Miguel Duarte Barroso <mdbarroso@redhat.com>
2023-10-04 15:48:24 +0200

    macvlan: unit test
    Add unit test asserting a macvlan interface can be added / retrieved /
    deleted and the expected parameters are there.
    
    Some default settings are set by the kernel - i.e. Flags, BcQueueLen,
    BcQueueLenUsed, and MacAddrCount.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- src/link/test.rs
+++ src/link/test.rs
@@ -2,7 +2,7 @@
 
 use futures::stream::TryStreamExt;
 use netlink_packet_route::link::{
-    InfoKind, LinkAttribute, LinkInfo, LinkMessage,
+    InfoData, InfoKind, InfoMacVlan, LinkAttribute, LinkInfo, LinkMessage,
 };
 use tokio::runtime::Runtime;
 
@@ -16,14 +16,65 @@ fn create_get_delete_wg() {
     let handle = rt.block_on(_create_wg());
     assert!(handle.is_ok());
     let mut handle = handle.unwrap();
-    let msg = rt.block_on(_get_wg(&mut handle));
+    let msg = rt.block_on(_get_iface(&mut handle, IFACE_NAME.to_owned()));
     assert!(msg.is_ok());
     let msg = msg.unwrap();
     assert!(has_nla(
         &msg,
         &LinkAttribute::LinkInfo(vec![LinkInfo::Kind(InfoKind::Wireguard)])
     ));
-    rt.block_on(_del_wg(&mut handle, msg.header.index)).unwrap();
+    rt.block_on(_del_iface(&mut handle, msg.header.index))
+        .unwrap();
+}
+
+#[test]
+fn create_get_delete_macvlan() {
+    const MACVLAN_IFACE_NAME: &str = "mvlan1";
+    const LOWER_DEVICE_IDX: u32 = 2;
+    const MACVLAN_MODE: u32 = 4; // bridge
+    let mac_address = &vec![2u8, 0, 0, 0, 0, 1];
+
+    let rt = Runtime::new().unwrap();
+    let handle = rt.block_on(_create_macvlan(
+        &MACVLAN_IFACE_NAME.to_owned(),
+        LOWER_DEVICE_IDX, // assuming there's always a network interface in the system ...
+        MACVLAN_MODE,
+        mac_address.to_vec(),
+    ));
+    assert!(handle.is_ok());
+
+    let mut handle = handle.unwrap();
+    let msg =
+        rt.block_on(_get_iface(&mut handle, MACVLAN_IFACE_NAME.to_owned()));
+    assert!(msg.is_ok());
+    assert!(has_nla(
+        &msg.as_ref().unwrap(),
+        &LinkAttribute::LinkInfo(vec![
+            LinkInfo::Kind(InfoKind::MacVlan),
+            LinkInfo::Data(InfoData::MacVlan(vec![
+                InfoMacVlan::Mode(MACVLAN_MODE),
+                InfoMacVlan::Flags(0), // defaulted by the kernel
+                InfoMacVlan::MacAddrCount(0), // defaulted by the kernel
+                InfoMacVlan::BcQueueLen(1000), // defaulted by the kernel
+                InfoMacVlan::BcQueueLenUsed(1000)  // defaulted by the kernel
+            ]))
+        ])
+    ));
+    assert!(has_nla(
+        &msg.as_ref().unwrap(),
+        &LinkAttribute::IfName(MACVLAN_IFACE_NAME.to_string())
+    ));
+    assert!(has_nla(
+        &msg.as_ref().unwrap(),
+        &LinkAttribute::Link(LOWER_DEVICE_IDX)
+    ));
+    assert!(has_nla(
+        &msg.as_ref().unwrap(),
+        &LinkAttribute::Address(mac_address.to_vec())
+    ));
+
+    rt.block_on(_del_iface(&mut handle, msg.unwrap().header.index))
+        .unwrap();
 }
 
 fn has_nla(msg: &LinkMessage, nla: &LinkAttribute) -> bool {
@@ -46,12 +97,38 @@ async fn _create_wg() -> Result<LinkHandle, Error> {
     Ok(link_handle)
 }
 
-async fn _get_wg(handle: &mut LinkHandle) -> Result<LinkMessage, Error> {
-    let mut links = handle.get().match_name(IFACE_NAME.to_owned()).execute();
+async fn _get_iface(
+    handle: &mut LinkHandle,
+    iface_name: String,
+) -> Result<LinkMessage, Error> {
+    let mut links = handle.get().match_name(iface_name).execute();
     let msg = links.try_next().await?;
     msg.ok_or(Error::RequestFailed)
 }
 
-async fn _del_wg(handle: &mut LinkHandle, index: u32) -> Result<(), Error> {
+async fn _del_iface(handle: &mut LinkHandle, index: u32) -> Result<(), Error> {
     handle.del(index).execute().await
+}
+
+async fn _create_macvlan(
+    name: &String,
+    lower_device_index: u32,
+    mode: u32,
+    mac: Vec<u8>,
+) -> Result<LinkHandle, Error> {
+    let (conn, handle, _) = new_connection().unwrap();
+    tokio::spawn(conn);
+    let link_handle = handle.link();
+    let mut req =
+        link_handle
+            .add()
+            .macvlan(name.to_string(), lower_device_index, mode);
+    req.message_mut()
+        .attributes
+        .push(LinkAttribute::IfName(name.to_owned()));
+    req.message_mut()
+        .attributes
+        .push(LinkAttribute::Address(mac));
+    req.execute().await?;
+    Ok(link_handle)
 }
