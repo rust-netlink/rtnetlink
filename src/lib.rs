a4d2611fb8c5fa1e5d045bf17215a7a7a25a480e
pseusys <shveitsar215@gmail.com>
2024-06-29 10:57:36 +0800

    `netlink-*` packages re-exported
    Signed-off-by: pseusys <shveitsar215@gmail.com>
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,12 @@
 
 #![allow(clippy::module_inception)]
 
+pub use netlink_packet_core as packet_core;
+pub use netlink_packet_route as packet_route;
+pub use netlink_packet_utils as packet_utils;
+pub use netlink_proto as proto;
+pub use netlink_sys as sys;
+
 mod handle;
 pub use crate::handle::*;
 
