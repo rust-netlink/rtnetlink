8e0e9ce45776ae2ae05fa63be965d4b54d1a415e
Renato Westphal <renato@opensourcerouting.org>
2025-05-10 20:21:48 -0300

    route: add support for multipath routes
    Add support for configuring multipath (ECMP) routes. This includes a
    new `RouteNextHopBuilder` for creating nexthop entries with various
    attributes.
    
    `RouteMessageBuilder` now has a `multipath()` method to attach multiple
    nexthops using the `RTA_MULTIPATH` attribute.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- src/route/mod.rs
+++ src/route/mod.rs
@@ -8,6 +8,7 @@ mod handle;
 
 pub use self::add::RouteAddRequest;
 pub use self::builder::RouteMessageBuilder;
+pub use self::builder::RouteNextHopBuilder;
 pub use self::del::RouteDelRequest;
 pub use self::get::{IpVersion, RouteGetRequest};
 pub use self::handle::RouteHandle;
